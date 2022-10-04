458903ef185084f6e1b2cdf2f300289a3e20a5e6
Yusuf Hasan <62721671+yh-Yusuf@users.noreply.github.com>
2022-10-04 16:57:25 +0530

    Update views.py

--- day2/myapp/views.py
+++ day2/myapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import *
+from django.http import *
 # Create your views here.
+
+def index(request):
+    d = Post.objects.all()
+
+    for i in d:
+        print(i.title)
+        print(i.text)
+
+    return HttpResponse("Hello !")
